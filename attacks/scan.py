7693837dedbd08d1b5a4fc02d964a595e4d5a3c0
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2024-05-15 17:24:34 +0200

    fix srp call

--- attacks/scan.py
+++ attacks/scan.py
@@ -4,12 +4,14 @@ network = '10.1.0.0/24'
 
 def arp_scan():
     arp_request = Ether(dst="ff:ff:ff:ff:ff:ff") / ARP(pdst=network)
-    result = srp(arp_request, iface="eth0", timeout=1, verbose=False)[0]
+
+    result = srp(arp_request, timeout=1)[0]
+    print(result)
 
     for device in result:
         print(device[1].psrc, device[1].hwsrc)
 
 if __name__ == "__main__":
     print("Scanning network...")
-    scan_results = arp_scan()
+    arp_scan()
     print("Scan complete.")
