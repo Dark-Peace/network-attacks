bb6e522cd5375c98e507cd2085ac976d92f24a54
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2024-05-16 11:20:51 +0200

    arp poison & ftp bruteforce defense finally ok

--- defenses/arp_poison.sh
+++ defenses/arp_poison.sh
@@ -1,4 +1,1 @@
-# for ws2 and ws3
-nft add table arp filter
-nft add chain arp filter input '{type filter hook input priority 0; policy accept;}'
-nft add rule arp filter input type arp request limit rate 5/minute accept
+nft add table arp filter; nft add chain arp filter input '{type filter hook input priority 0; policy accept;}'; nft add rule arp filter input arp operation request limit rate 5/minute drop
--- defenses/ftp_bruteforce.sh
+++ defenses/ftp_bruteforce.sh
@@ -1,3 +1,1 @@
-# for ftp
-nft add chain inet filter input_ftp '{type filter hook input priority 0;}'
-nft add rule inet filter input_ftp tcp dport 21 limit rate over 3/minute burst 1 packets counter drop
+nft add chain inet ftp_filter ftp_input '{type filter hook input priority 0; policy accept;}'; nft add rule inet ftp_filter ftp_input tcp dport 21 limit rate over 3/minute burst 1 packets counter drop
