f2189e90d632ee88be69cf8bf4a3fa9763f99814
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2024-05-16 12:07:22 +0200

    accept by default

--- defenses/reflected_dns_dos.sh
+++ defenses/reflected_dns_dos.sh
@@ -1,2 +1,2 @@
-nft add chain inet dns_filter input '{type filter hook input priority 0; policy drop;}'
+nft add chain inet dns_filter input '{type filter hook input priority 0; policy accept;}'
 nft add rule inet dns_filter input udp dport 53 limit rate over 3/minute burst 1 packets counter drop
