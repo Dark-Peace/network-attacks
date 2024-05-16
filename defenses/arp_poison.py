fa5dbe8222de88bcaa0d24c7e9c15f47c7aa3e2e
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2024-05-16 10:24:51 +0200

    Delete defenses/arp_poison.py

--- defenses/arp_poison.py
+++
@@ -1,8 +0,0 @@
-from topo import net
-from mininet.log import info
-
-if __name__ == "__main__":
-    for pc in ["ws2","ws3"]:
-        info(net[pc].cmd("nft add table arp filter"))
-        info(net[pc].cmd("nft add chain arp filter input {type filter hook input priority 0; policy accept;}"))
-        info(net[pc].cmd("nft add rule arp filter input type arp request limit rate 5/minute accept"))
