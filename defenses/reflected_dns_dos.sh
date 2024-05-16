22fe96e8529dfe70cc932cfee9d02d17a4f4f2bb
Dark Peace <74102789+Dark-Peace@users.noreply.github.com>
2024-05-16 11:30:05 +0200

    change back for new topo.py

--- defenses/reflected_dns_dos.sh
+++ defenses/reflected_dns_dos.sh
@@ -1,2 +1,2 @@
-nft add chain inet filter input '{type filter hook input priority 0; policy drop;}'
-nft add rule inet filter input udp dport 53 limit rate over 3/minute burst 1 packets counter drop
+nft add chain inet dns_filter input '{type filter hook input priority 0; policy drop;}'
+nft add rule inet dns_filter input udp dport 53 limit rate over 3/minute burst 1 packets counter drop
